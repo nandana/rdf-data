91645e6d4481cd96bc8f1754a34bbd5df0a88b8b
Nandana Mihindukulasooriya <nandana.cse@gmail.com>
2013-02-22 21:29:31 +0100

    minor formatting

--- oeg/homepages/email.py
+++ oeg/homepages/email.py
@@ -34,11 +34,13 @@ r = re.compile(r'(\b[\w.]+@+[\w.]+.+[\w.]\b)')
 results = r.findall(bulkemails)    
 
 emails = ""  
-table = PrettyTable(["id", "/~{id}[1]", "/members/{id}[2]", "Found", "http://www.oeg-upm.net/"]) 
+table = PrettyTable(["id", "/~{id}       " , "/members/{id}", "Homepage"]) 
 table.align["id"] = "l"
 table.align["domain"] = "l"  
-table.align["Found"] = "l"
-table.align["http://www.oeg-upm.net/"] = "l" 
+table.align["Homepage"] = "l"
+table.hrule = 2
+table.vrule = 1
+
 for x in results:
 	email = str(x)
 	splited = email.split('@')
@@ -49,22 +51,31 @@ for x in results:
 	url3 = "http://delicias.dia.fi.upm.es/members/" + nick[:2].upper() + nick[2:] + "/"
 	url4 = ""
 	page = ""
+	pattern1 = ""
+	pattern2 = ""
 	check1 = checkUrl(url1)
 	if check1:
 		page = url1
+		pattern1 = "Found"
         check2 = checkUrl(url2)
 	if check2:
 		page = url2
+		pattern2 = "Found"
         check3 = checkUrl(url3)
 	if check3:
 		page = url3
+		pattern2 = "Found"
 	check2 = check2 or check3
 	for line in open("oeg_net_page.txt"):
 		if nick in line:
 	   		url4 = line[:-1]
-        table.add_row([nick, check1, check2, str(page), str(url4)])
+        if page:
+		urls = str(page) + "\n" + str(url4) 
+	else:
+		urls = str(url4)
+        table.add_row([nick, pattern1, pattern2, urls ])
 	
-print table
+print table.get_string(sortby="id")
 
 print "[1] - http://delicias.dia.fi.upm.es/~{id}"
 print "[2] - http://delicias.dia.fi.upm.es/members/{id}" 
